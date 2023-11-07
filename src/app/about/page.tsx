64a75ec3053a29becfeaf4bb19555c564ed4f6e7
Rushikesh Pandit <13253098+rushikeshpandit@users.noreply.github.com>
2023-11-07 17:12:49 +0530

    Pn 4 build about page (#2)
    * - Updated nav bar menu text color
    
    * - Updated side bar menu text color
    
    * - Updated home page text color
    
    * - Completed about page
    
    * - Added into file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,14 @@
+import {info} from '../info/Info';
+
 export default function About() {
   return (
-    <main className="bg-red flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-black">About page from folder</p>
+    <main>
+      <div className="pt-10 h-screen container my-auto mx-auto bg-white items-center justify-evenly flex-col">
+        <h2 className="text-[#42446E]  font-bold, text-5xl">About Me</h2>
+        <div className="pt-10 pb-10 w-auto mx-auto">
+          <p className=" break-words text-[#666666] whitespace-pre-line text-xl">{info.bio}</p>
+        </div>
+      </div>
     </main>
   );
 }
