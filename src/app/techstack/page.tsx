a0bcac7667edcddb7be6146eeec5820974016464
Rushikesh Pandit <13253098+rushikeshpandit@users.noreply.github.com>
2023-11-07 22:54:14 +0530

    Pn 5 build tech stack page (#3)
    * - Added images for tech stack
    
    * - Added tech stack to info
    
    * - Tech stack page
    
    * - Code lint

--- src/app/techstack/page.tsx
+++ src/app/techstack/page.tsx
@@ -1,7 +1,21 @@
+import {info} from '../info/Info';
+import Image from 'next/image';
+
 export default function TechStack() {
   return (
-    <main className="bg-red flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-black">Techstack page from folder</p>
+    <main>
+      <div className="pt-10 h-screen container my-auto mx-auto bg-white items-center justify-evenly flex-col">
+        <h2 className="text-[#42446E]  font-bold, text-5xl">My Tech Stack</h2>
+        <h3 className="text-[#666666]  font-bold, text-xl pt-10 pb-10">Technologies I've been working with recently</h3>
+        <div className="p-10 h-auto w-auto mx-auto flex flex-wrap">
+          {info.techStack.map(({name, image}) => (
+            <div key={name} className="w-18 h-18 m-12 flex-col flex items-center justify-center">
+              <Image src={image} alt={name} width={80} height={80} />
+              <p className="text-[#666666] mt-4">{name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
