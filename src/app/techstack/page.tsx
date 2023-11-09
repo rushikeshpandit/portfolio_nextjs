e72938dc35e02d0fa1df043cef67b9d39ceb4ccc
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-11-09 14:56:02 +0530

    - Code lint and minor correction

--- src/app/techstack/page.tsx
+++ src/app/techstack/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function TechStack() {
   return (
     <main>
-      <div className="pt-10 h-screen container my-auto mx-auto bg-white items-center justify-evenly flex-col">
+      <div className="pt-10 h-auto container my-auto mx-auto bg-white items-center justify-evenly flex-col">
         <h2 className="text-[#42446E]  font-bold, text-5xl">My Tech Stack</h2>
         <h3 className="text-[#666666]  font-bold, text-xl pt-10 pb-10">Technologies I've been working with recently</h3>
         <div className="p-10 h-auto w-auto mx-auto flex flex-wrap">
