ad9e5d214840f219776b8238ae4a6738829de361
Rushikesh Pandit <13253098+rushikeshpandit@users.noreply.github.com>
2024-01-22 20:23:59 +0530

    - Added side projects feature (#6)

--- src/app/professional/page.tsx
+++ src/app/professional/page.tsx
@@ -1,5 +1,6 @@
 import {info} from '../info/Info';
 import Image from 'next/image';
+import Link from 'next/link';
 
 enum iconTypes {
   office = '/office.svg',
@@ -18,6 +19,26 @@ export default function Professional() {
         return <Image className="h-8 w-6" src={iconTypes.location} alt="Self" width={16} height={16} />;
     }
   };
+
+  const renderProject = () => (
+    <div className="h-auto w-auto flex flex-wrap flex-col">
+      {info.sideProjects.map((project, index) => (
+        <div key={project.name} className="my-4 flex items-left justify-between flex-col">
+          <p className="text-[#666666] my-4 font-normal text-2xl">
+            {index + 1 + '. '}
+            {project.name}
+          </p>
+
+          <p className="text-[#666666] my-4 font-normal text-2xl">Description</p>
+          <p className="text-[#A7A7A7] my-4 mx-4 font-light text-xl text-left">{project.description}</p>
+
+          <Link href={project.link}>
+            <p className="text-[#666666] my-4 font-normal text-2xl">Download Link</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 
   const renderTimeline = (isWork: boolean = true) => (
     <div className="h-auto w-auto flex flex-wrap flex-col">
@@ -123,6 +144,8 @@ export default function Professional() {
       <div className="pt-16 h-auto container my-auto mx-auto bg-white items-center justify-evenly flex-col">
         <h2 className="text-[#42446E]  font-bold, text-5xl">Work Experience</h2>
         {renderTimeline()}
+        <h2 className="text-[#42446E]  font-bold, text-5xl">Side Projects</h2>
+        {renderProject()}
         <h2 className="text-[#42446E]  font-bold, text-5xl">Education</h2>
         {renderTimeline(false)}
       </div>
