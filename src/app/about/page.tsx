0bf922f46c10c73947050f618ef08aa187d94d6d
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-09-28 15:41:16 +0530

    - Added height and width for "about" page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import {info} from '../info/Info';
 
 export default function About() {
   return (
-    <div id="about">
+    <div id="about" className="h-screen w-screen justify-center items-center">
       <div className="pt-16 h-auto container my-auto mx-auto bg-white items-center justify-evenly flex-col">
         <h2 className="text-[#42446E]  font-bold, text-5xl">About Me</h2>
         <div className="pt-10 pb-10 w-auto mx-auto">
