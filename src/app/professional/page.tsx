4a96320b58f2b7a0ddcc3fef4be0f4aea651a66f
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-09-28 15:44:00 +0530

    - Removed "Side Projects" section from "professional" page

--- src/app/professional/page.tsx
+++ src/app/professional/page.tsx
@@ -20,30 +20,6 @@ export default function Professional() {
     }
   };
 
-  const renderProject = () => (
-    <div className="h-auto w-auto flex flex-wrap flex-col">
-      {info.sideProjects.map((project, index) => (
-        <div key={project.name} className="my-4 flex items-left justify-between flex-col">
-          <p className="text-[#666666] my-4 font-normal text-2xl">
-            {index + 1 + '. '}
-            {project.name}
-          </p>
-
-          <p className="text-[#666666] my-4 font-normal text-2xl">Description</p>
-          <p className="text-[#A7A7A7] my-4 mx-4 font-light text-xl text-left">{project.description}</p>
-
-          <Link href={project.link} rel="noopener noreferrer" target="_blank">
-            {project.isWebsite ? (
-              <p className="text-[#666666] my-4 font-normal text-2xl">Visit Website</p>
-            ) : (
-              <p className="text-[#666666] my-4 font-normal text-2xl">Download Link</p>
-            )}
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
-
   const renderTimeline = (isWork: boolean = true) => (
     <div className="h-auto w-auto flex flex-wrap flex-col">
       {info.experience.map(data =>
@@ -148,8 +124,6 @@ export default function Professional() {
       <div className="pt-16 h-auto container my-auto mx-auto bg-white items-center justify-evenly flex-col">
         <h2 className="text-[#42446E]  font-bold, text-5xl">Work Experience</h2>
         {renderTimeline()}
-        <h2 className="text-[#42446E]  font-bold, text-5xl">Side Projects</h2>
-        {renderProject()}
         <h2 className="text-[#42446E]  font-bold, text-5xl">Education</h2>
         {renderTimeline(false)}
       </div>
