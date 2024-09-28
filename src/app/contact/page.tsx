834ccd0d471893dfecbec4786e081ddcb83e03cc
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-09-28 15:41:26 +0530

    - Added height and width for "contact" page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Contact() {
   return (
-    <div id="contact">
+    <div id="contact" className="h-screen w-screen justify-center items-center">
       <div className="pt-16 h-auto container my-auto mx-auto bg-white items-center justify-evenly flex-col">
         <h2 className="text-[#42446E]  font-bold, text-5xl">Connect with me</h2>
         <div className="p-10 h-auto w-auto mx-auto flex flex-wrap items-center justify-center">
