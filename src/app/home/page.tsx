47c4a2a867ef5be8ca8336dc9cb05869abadacaf
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-09-30 11:04:09 +0530

    - Added animation to name

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,53 @@
+'use client';
+
 import Image from 'next/image';
+import React, {useState, useCallback, useRef, useEffect} from 'react';
+import {useTransition, animated} from '@react-spring/web';
+import styles from './styles.module.css';
 
 export default function Home() {
+  const ref = useRef<ReturnType<typeof setTimeout>[]>([]);
+  const [item, set] = useState<string>('Rushikesh Pandit');
+  const transitions = useTransition(item, {
+    from: {
+      opacity: 0,
+      height: 0,
+      innerHeight: 0,
+      transform: 'perspective(600px) rotateX(0deg)',
+    },
+    enter: [
+      {opacity: 1, height: 80, innerHeight: 80},
+      {transform: 'perspective(600px) rotateX(180deg)'},
+      {transform: 'perspective(600px) rotateX(0deg)'},
+    ],
+    leave: [{innerHeight: 80}, {opacity: 0, height: 80}],
+  });
+  const reset = useCallback(() => {
+    ref.current.forEach(clearTimeout);
+    ref.current = [];
+    ref.current.push(setTimeout(() => set('Rushikesh Pandit'), 2000));
+  }, []);
+
+  useEffect(() => {
+    reset();
+    return () => ref.current.forEach(clearTimeout);
+  }, []);
+
   const renderText = () => {
     return (
       <div>
         <p className="mx-20 text-[#42446E] text-7xl font-bold">Hi 👋 , </p>
-        <p className="mx-20 text-[#42446E] text-7xl font-bold">My name is </p>
-        <h1 className="mx-20 text-7xl font-bold bg-gradient-to-r from-blue-400 to-indigo-600 inline-block text-transparent bg-clip-text">
-          Rushikesh Pandit
-        </h1>
-        <p className="mx-20 text-[#42446E] text-7xl font-bold">I build things for </p>
+        <p className="mx-20 mb-5 text-[#42446E] text-7xl font-bold">My name is </p>
+        {transitions(({innerHeight, ...rest}, item) => (
+          <animated.div className={styles.transitionsItem} style={rest} onClick={reset}>
+            <animated.div style={{overflow: 'hidden', height: innerHeight}}>
+              <h1 className="mx-20 text-7xl font-bold bg-gradient-to-r from-blue-400 to-indigo-600 inline-block text-transparent bg-clip-text">
+                {item}
+              </h1>
+            </animated.div>
+          </animated.div>
+        ))}
+        <p className="mx-20 mt-5 text-[#42446E] text-7xl font-bold">I build things for </p>
         <p className="mx-20 text-[#42446E] text-7xl font-bold">web and mobile</p>
       </div>
     );
